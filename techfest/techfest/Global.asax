cfd586e6c84bfd1392eac6fed926962a4ff77e9a
Adrian Wright <adriantwright@gmail.com>
2014-06-09 08:07:01 -0400

    add for the 1st time

---
+++ techfest/techfest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="techfest.MvcApplication" Language="C#" %>
---
+++ techfest/techfest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(techfest.Startup))]
+namespace techfest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
